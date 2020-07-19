501a1b2bc0c0d70593e2f612c23069bed8ae4efd
Prashant Jha <jha.prashant654@gmail.com>
2020-07-19 16:14:23 +0530

    Final commit with modified icons

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import { Scoreboard } from './components/scoreboard'
 import './styles/board.css';
 import './styles/box.css';
 import './styles/button.css';
-// Create App component
+
+
 class App extends React.Component {
   render() {
     return (
